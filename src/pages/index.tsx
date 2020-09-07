66340804259310e2618168d921192d6ae46c9edf
sadnessOjisan <sadness.ojisan@gmail.com>
2020-09-08 04:26:34 +0900

    add stripe code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react'
-// import { Elements, CardElement } from '@stripe/react-stripe-js';
-// import { loadStripe } from '@stripe/stripe-js';
+import { Elements, CardElement } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 const videoId = 'sample'
 
-const Movies = () => {
-    // const stripeApiKey = 'pk_test_51HLU77BUWDSmzbsOYdzhlBW8pFrZ6lZKYMOjyJVc4OeUDPa3TdXVFW4VhfOzXxKKwyOetwCGF4nuFsglRGeKOLqB00yjJhJ12C'
-    // const stripePromise = loadStripe(stripeApiKey);
-    // React.useEffect(() => {
+const Payment = () => {
+    const stripeApiKey = 'pk_test_51HLU77BUWDSmzbsOYdzhlBW8pFrZ6lZKYMOjyJVc4OeUDPa3TdXVFW4VhfOzXxKKwyOetwCGF4nuFsglRGeKOLqB00yjJhJ12C'
+    const stripePromise = loadStripe(stripeApiKey);
+    React.useEffect(() => {
 
-    // }, [])
+    }, [])
     return <div>
-        {/* <div style={{ maxWidth: 760 }}>
+        <div style={{ maxWidth: 760 }}>
             <Elements stripe={stripePromise}>
                 <CheckoutForm></CheckoutForm>
-            </Elements></div> */}
-            hello
+            </Elements></div>
     </div>
 }
 
-export default Movies
+const CheckoutForm = () => {
+    return <CardElement></CardElement>
+}
+
+export default Payment
